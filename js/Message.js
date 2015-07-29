86361b04e34f7f815d29ce435b95793578e630c9
Burtchen <Burtchen@web.de>
2015-07-29 10:05:16 +0200

    rework setState after asynchronous request, include basic setup for searches yielding no result, minor markup change

--- js/Message.js
+++ js/Message.js
@@ -24,6 +24,7 @@ var Message = React.createClass({
         var searchKeywords = this.state.text.split(" ");
         searchKeywords.forEach(function (keyword) {
             var request = new XMLHttpRequest();
+            var firstMatch;
             request.open('GET', spotifySearchUrl + "?q=" + keyword + "&type=track", true);
 
             request.onreadystatechange = function() {
@@ -34,18 +35,28 @@ var Message = React.createClass({
                         // TODO: Multiple matches
                         if (data.tracks && data.tracks.items) {
                             _.each(data.tracks.items, function (item) {
+                                firstMatch = null;
                                 if (item.name.toLowerCase() === keyword.toLowerCase()) {
-                                    incomingSongs.push({
-                                        id: item.id,
-                                        title: item.name,
-                                        artist: item.artists[0].name
-                                    });
+                                    firstMatch = item;
                                     return false;
                                 }
                             });
+                            if (firstMatch) {
+                                incomingSongs.push({
+                                    id: firstMatch.id,
+                                    title: firstMatch.name,
+                                    artist: firstMatch.artists[0].name
+                                });
+                            } else {
+                                incomingSongs.push({
+                                    title: keyword,
+                                    nomatch: true
+                                });
+                            }
                         }
+                        that.setState({songs: incomingSongs});
                     } else {
-                        // Error :(
+                        that.setState({generalError: true});
                     }
                 }
             };
@@ -53,11 +64,10 @@ var Message = React.createClass({
             request.send();
             request = null;
         });
-        that.setState({songs: incomingSongs})
     },
-    eachSong: function(song, i) {
+    eachSong: function(song) {
         return (
-            <Song title={song.title} artist={song.artist} key={song.id}/>
+            <Song title={song.title} artist={song.artist} key={song.id} nomatch={song.nomatch}/>
         );
     },
     handleChange: function(event) {
@@ -76,12 +86,14 @@ var Message = React.createClass({
                             onClick={this.getSongsForPlaylist}
                             disabled={this.state.text.length === 0}>Get songs for playlist</button>
                 </div>
-                <ul id="react-suggested-songs" className="clearfix list-group">
-                    {this.state.songs.map(this.eachSong)}
-                </ul>
-                <button className="btn btn-primary pull-right"
-                        onClick={this.createPlaylist}
-                        disabled={this.state.songs.length === 0}>Create playlist</button>
+                <div className="well clearfix">
+                    <ul id="react-suggested-songs" className="clearfix list-group">
+                        {this.state.songs.map(this.eachSong)}
+                    </ul>
+                    <button className="btn btn-primary pull-right"
+                            onClick={this.createPlaylist}
+                            disabled={this.state.songs.length === 0}>Create playlist</button>
+                </div>
             </div>
         );
     }
--- js/Song.js
+++ js/Song.js
@@ -1,10 +1,18 @@
 var Song = React.createClass({
     render: function() {
-        return (
-            <li className="list-group-item">
-                {this.props.title}, {this.props.artist}
-            </li>
-        );
+        if (this.props.nomatch) {
+            return (
+                <li className="list-group-item list-group-item-warning">
+                    We could not find any matches for {this.props.title}.
+                </li>
+            );
+        } else {
+            return (
+                <li className="list-group-item">
+                    {this.props.title}, {this.props.artist}
+                </li>
+            );
+        }
     }
 
 });
