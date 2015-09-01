7eb032e22f3d3353faa05fedda77225d6c70452f
Burtchen <Burtchen@web.de>
2015-09-01 09:02:02 +0200

    enabled actual playlist creation, improved stability of list, update status in Song items, add watcher (to be run with npm run watch)

--- js/Message.js
+++ js/Message.js
@@ -5,32 +5,66 @@ var Message = React.createClass({
             songs: []
         };
     },
-    createPlaylist: function () {
-        var spotifyAuthUrl = "https://accounts.spotify.com/authorize";
-        $.get(spotifyAuthUrl, {
-                "client_id": "14ab7c0b9c0b4c7d982b50a0eb7f8e8a",
-                "response_type": "token", //code
-                "redirect_uri": "http://localhost:4321" //current location?
-            }, function (data) {
-        });
-        // create a new empty playlist for the user
-        // populate it with all the songs
-        // TODO: explanation text, all the possible errors
+    addSongsToPlaylist: function (playlistId, accessToken) {
+        var request = new XMLHttpRequest();
+        var userId = 'burtchen'; //TODO: Get from user request
+        var uris = _.pluck(this.state.songs, "uri"); //TODO: No undefineds.
+        request.open('POST', 'https://api.spotify.com/v1/users/' + userId + '/playlists/' + playlistId + '/tracks', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.setRequestHeader("Authorization", "Bearer " + accessToken);
+        request.send(JSON.stringify(uris));
+        request.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status >= 200 && this.status < 400) {
+                    var responseObject = JSON.parse(this.responseText);
+                    console.log('playlist has been filled');
+                } else {
+                    console.log('yikes, an error adding stuff to the playlist');
+                }
+            }
+        };
+    },
+    createPlaylist: function (accessToken) {
+        var that = this;
+        var request = new XMLHttpRequest();
+        var userId = 'burtchen'; //TODO: Get from user request
+        var data = {
+            name: "messageForYou" // TODO: Make playlist name customizable
+        };
+        request.open('POST', 'https://api.spotify.com/v1/users/' + userId + '/playlists', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.setRequestHeader("Authorization", "Bearer " + accessToken);
+        request.send(JSON.stringify(data));
+        request.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status >= 200 && this.status < 400) {
+                    var responseObject = JSON.parse(this.responseText);
+                    that.addSongsToPlaylist(responseObject.id, accessToken);
+                } else {
+                    console.log('There was an error creating the playlist.');
+                }
+            }
+        };
     },
     getSongsForPlaylist: function() {
         var that = this;
         var incomingSongs = [];
         var spotifySearchUrl = "https://api.spotify.com/v1/search";
-        var searchKeywords = this.state.text.split(" ");
+        var searchKeywords = this.state.text.split(" "); //TODO: Custom delimiters, eliminate spaces.
         searchKeywords.forEach(function (keyword) {
             var request = new XMLHttpRequest();
+            incomingSongs.push({
+                title: keyword,
+                status: "pending"
+            });
             var firstMatch;
+            that.setState({songs: incomingSongs});
             request.open('GET', spotifySearchUrl + "?q=" + keyword + "&type=track", true);
-
             request.onreadystatechange = function() {
                 if (this.readyState === 4) {
                     if (this.status >= 200 && this.status < 400) {
                         var data = JSON.parse(this.responseText);
+                        var songObject; //TODO: let;
                         // TODO: Filter by market
                         // TODO: Multiple matches
                         if (data.tracks && data.tracks.items) {
@@ -41,17 +75,15 @@ var Message = React.createClass({
                                     return false;
                                 }
                             });
+                            songObject = _.findWhere(incomingSongs, {title: keyword});
                             if (firstMatch) {
-                                incomingSongs.push({
-                                    id: firstMatch.id,
-                                    title: firstMatch.name,
-                                    artist: firstMatch.artists[0].name
-                                });
+                                songObject.status = "match";
+                                songObject.id = firstMatch.id;
+                                songObject.url = firstMatch.uri;
+                                songObject.title = firstMatch.name;
+                                songObject.artist = firstMatch.artists[0].name;
                             } else {
-                                incomingSongs.push({
-                                    title: keyword,
-                                    nomatch: true
-                                });
+                                songObject.status = "unmatched";
                             }
                         }
                         that.setState({songs: incomingSongs});
@@ -67,11 +99,68 @@ var Message = React.createClass({
     },
     eachSong: function(song) {
         return (
-            <Song title={song.title} artist={song.artist} key={song.id} nomatch={song.nomatch}/>
+            <Song title={song.title} artist={song.artist} key={song.id} status={song.status}/>
         );
     },
     handleChange: function(event) {
         this.setState({ text: event.target.value });
+    },
+
+    getSpotifyApi: function() { //TODO: If still logged in, we should directly go to the playlist creation
+        var that = this;
+        var CLIENT_ID = '14ab7c0b9c0b4c7d982b50a0eb7f8e8a';
+        var REDIRECT_URI = 'https://www.der-burtchen.de/playlistmessage/proxy/';
+        function getLoginURL(scopes) {
+            return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+                '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+                '&scope=' + encodeURIComponent(scopes.join(' ')) +
+                '&response_type=token';
+        }
+
+        var url = getLoginURL([
+            'playlist-modify-public' //TODO: check whether private is better/worse
+        ]);
+
+        var width = 450,
+            height = 730,
+            left = (screen.width / 2) - (width / 2),
+            top = (screen.height / 2) - (height / 2);
+
+        var messageCallback =  function(event) {
+            var hash = JSON.parse(event.data);
+            if (hash.type == 'access_token') {
+                that.getUserData(hash.access_token); //TODO: Maybe still display user data
+                that.createPlaylist(hash.access_token);
+            }
+            window.removeEventListener("message", messageCallback);
+        };
+
+        window.addEventListener("message", messageCallback.bind(this), false);
+
+        var w = window.open(url,
+            'Spotify',
+            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+        );
+
+    },
+
+    getUserData: function(accessToken) {
+        var request = new XMLHttpRequest();
+        request.open('GET', 'https://api.spotify.com/v1/me', true);
+        request.setRequestHeader("Authorization", "Bearer " + accessToken);
+        request.onreadystatechange = function() {
+            if (this.readyState === 4) {
+                if (this.status >= 200 && this.status < 400) {
+                    var resp = this.responseText;
+                    console.log(resp);
+                } else {
+                    console.log("yikes, an error");
+                }
+            }
+        };
+
+        request.send();
+        request = null;
     },
     render: function() {
         return (
@@ -91,7 +180,7 @@ var Message = React.createClass({
                         {this.state.songs.map(this.eachSong)}
                     </ul>
                     <button className="btn btn-primary pull-right"
-                            onClick={this.createPlaylist}
+                            onClick={this.getSpotifyApi}
                             disabled={this.state.songs.length === 0}>Create playlist</button>
                 </div>
             </div>
--- js/Song.js
+++ js/Song.js
@@ -1,15 +1,22 @@
 var Song = React.createClass({
     render: function() {
-        if (this.props.nomatch) {
+        if (this.props.status === "pending") {
+            return (
+                <li className="list-group-item list-group-item-info">
+                    We are looking for potential matches for &quot;{this.props.title}&quot;.
+                </li>
+            );
+        }
+        if (this.props.status === "unmatched") {
             return (
                 <li className="list-group-item list-group-item-warning">
-                    We could not find any matches for {this.props.title}.
+                    We could not find any matches for &quot;{this.props.title}&quot;.
                 </li>
             );
         } else {
             return (
-                <li className="list-group-item">
-                    {this.props.title}, {this.props.artist}
+                <li className="list-group-item list-group-item-success">
+                    <strong>{this.props.title}</strong> <small>by {this.props.artist}</small>
                 </li>
             );
         }
